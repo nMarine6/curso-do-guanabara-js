1afef1ab568bd848b9d1ed1a0cd0e1d0f969db54
nMarine6 <naemarine6@gmail.com>
2025-08-13 15:54:11 -0300

    AAAAAAAAAAAAAAAAAA

--- Desafio/mda20/script.js
+++ Desafio/mda20/script.js
@@ -9,6 +9,45 @@ function veri(){
     } else {
         var fgen = document.getElementsByName('radgen')
         var age = year - Number(fyear.value)
+        var img = document.createElement(img)
+        img.setAttribute('id', 'foto')
+        
         res.innerHTML = `<p>${age}</p>`
+
+        var gender = ''
+
+        if (fgen[0].checked) {
+            gender = 'Homem'
+            if (age < 31) {
+                img.setAttribute('src', 'masc20y.png')
+            } else if (age < 41) {
+                img.setAttribute('src', 'masc30y.png')
+            } else if (age < 51) {
+                img.setAttribute('src', 'masc40y.png')
+            } else if (age < 61) {
+                img.setAttribute('src', 'masc50y.png')
+            } else if (age < 71) {
+                img.setAttribute('src', 'masc60y.png')
+            } else {
+                img.setAttribute('src', 'masc70y.png')
+            }
+        } else if (fgen[1].checked) {
+            gender = 'Mulher'
+            if (age < 31) {
+
+            } else if (age < 41) {
+
+            } else if (age < 51) {
+
+            } else if (age < 61) {
+
+            } else if (age < 71) {
+
+            } else {
+
+            }
+        }
+        res.innerHTML = `Achamos ${gender} com ${age} anos.`
+        res.appendChild(img)
     }
 }
